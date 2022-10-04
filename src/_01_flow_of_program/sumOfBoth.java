57292d295688f756de583a0cc294a6dcba949bf8
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-10-04 16:22:19 +0600

    Sum of both

---
+++ src/_01_flow_of_program/sumOfBoth.java
@@ -0,0 +1,16 @@
+package _01_flow_of_program;
+
+import java.util.Scanner;
+
+public class sumOfBoth {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a, b;
+        System.out.print("Enter the first number: ");
+        a = in.nextInt();
+        System.out.print("Enter the second number: ");
+        b = in.nextInt();
+
+        System.out.println("Sum of both : " + (a + b));
+    }
+}
