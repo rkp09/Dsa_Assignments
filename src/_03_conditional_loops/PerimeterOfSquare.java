2aba0f32c8d2a5b52589c9d91cb34d8f98031d59
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-10-05 13:48:27 +0600

    Calculating the perimeter of square.

---
+++ src/_03_conditional_loops/PerimeterOfSquare.java
@@ -0,0 +1,21 @@
+package _03_conditional_loops;
+
+import java.util.Scanner;
+
+public class PerimeterOfSquare {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // Formula for perimeter of square is
+        // perimeter = 2 * a (a = side length)
+
+        // Input for the length of the square
+        System.out.print("Enter the side length of the rectangle: ");
+        double a = in.nextDouble();
+
+        // Perimeter of square
+        double perimeter = 4 * a;
+
+        System.out.println("Perimeter of square is " + perimeter);
+    }
+}
