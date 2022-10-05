d791d9fa8425f9d12763126af25382891bf6c060
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-10-05 13:52:58 +0600

    Calculating the perimeter of rhombus.

--- src/_03_conditional_loops/PerimeterOfRhombus.java
+++ src/_03_conditional_loops/PerimeterOfRhombus.java
@@ -6,16 +6,16 @@ public class PerimeterOfRhombus {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // Formula for perimeter of square is
+        // Formula for perimeter of rhombus is
         // perimeter = 2 * a (a = side length)
 
-        // Input for the length of the square
-        System.out.print("Enter the side length of the rectangle: ");
+        // Input for the length of the rhombus
+        System.out.print("Enter the side length of the rhombus: ");
         double a = in.nextDouble();
 
-        // Perimeter of square
+        // Perimeter of rhombus
         double perimeter = 4 * a;
 
-        System.out.println("Perimeter of square is " + perimeter);
+        System.out.println("Perimeter of rhombus is " + perimeter);
     }
 }
