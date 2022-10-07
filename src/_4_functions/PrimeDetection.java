6a687015e5f446fe9aab8a4834bf9ea164f14b46
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-10-07 14:50:54 +0600

    Determine whether the prime or not.

---
+++ src/_4_functions/PrimeDetection.java
@@ -0,0 +1,29 @@
+package _4_functions;
+
+import java.util.Scanner;
+
+public class PrimeDetection {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // Enter a number
+        System.out.print("Enter a number: ");
+        int n = in.nextInt();
+
+        if(isPrime(n))
+            System.out.println(n + " is a prime number.");
+        else
+            System.out.println(n + " is not a prime number.");
+    }
+
+    public static boolean isPrime(int n){
+        int i = 2;
+
+        while(i <= n / i){
+            if (n % i == 0)
+                return false;
+            i++;
+        }
+
+        return true;
+    }
+}
