535da782b561c806b19e413e629dd2afe16dc535
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-10-18 09:05:10 +0600

    Find the greatest number which is smaller than or equal to target.

---
+++ src/_6_searching/FloorOfTarget.java
@@ -0,0 +1,27 @@
+package _6_searching;
+
+public class FloorOfTarget {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 5, 7, 9, 11, 13, 15, 18, 19, 21};
+        System.out.println(floor(nums, 9));
+    }
+
+    public static int floor(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] < target){
+                start = mid + 1;
+            }else if(nums[mid] > target){
+                end = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+
+        return end;
+    }
+}
