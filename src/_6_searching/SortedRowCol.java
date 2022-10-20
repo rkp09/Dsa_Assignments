b2d230ebe33060b337264c72992c0e4dfbd1bf08
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-10-20 10:23:51 +0600

    Search in matrix in which has sorted row and sorted column.

---
+++ src/_6_searching/SortedRowCol.java
@@ -0,0 +1,39 @@
+package _6_searching;
+
+import java.util.Arrays;
+
+public class SortedRowCol {
+    public static void main(String[] args) {
+        // Search in sorted row and sorted column matrix
+        int[][] matrix = {
+                {10, 15, 28, 40},
+                {12, 17, 32, 44},
+                {13, 21, 34, 46},
+                {14, 24, 39, 50}
+        };
+
+        System.out.println(Arrays.toString(search(matrix, 14)));
+    }
+
+
+    public static int[] search(int[][] matrix, int target){
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] == target){
+                return new int[] {row, col};
+            }
+
+            if(matrix[row][col] > target){
+                col--;
+            }
+
+            if(matrix[row][col] < target){
+                row++;
+            }
+        }
+
+        return new int[] {-1, -1};
+    }
+}
