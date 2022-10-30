6c7589807f6a7450a37bffdc22d3daddc7e1dbc0
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-10-30 19:20:52 +0600

    Pattern eleven.

---
+++ src/_8_patterns/Pattern_11.java
@@ -0,0 +1,23 @@
+package _8_patterns;
+
+public class Pattern_11 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    public static void pattern(int n){
+        // Running number of row times.
+        for (int row = 0; row < n; row++) {
+            // Creating number of space required to align it to the right side
+            for (int i = 1; i <= row; i++) {
+                System.out.print(" ");
+            }
+
+            // Running number of row times to print such column.
+            for (int col = n; col > row; col--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
