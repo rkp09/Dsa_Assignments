e3acbd3b07049849bbe621bad50746079b9391b9
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-10-30 13:34:47 +0600

    Pattern three.

---
+++ src/_8_patterns/Pattern_3.java
@@ -0,0 +1,20 @@
+package _8_patterns;
+
+public class Pattern_3 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    public static void pattern(int n){
+        // Printing the row
+        for (int i = n; i > 0; i--) {
+            // Printing the column of column in each row
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+
+            // Print a new line after printing a row
+            System.out.println();
+        }
+    }
+}
