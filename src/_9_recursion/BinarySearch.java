50fc64a397fe04af6498fd6305f70d04d14df87a
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-10-31 17:10:10 +0600

    Binary search using recursion.

---
+++ src/_9_recursion/BinarySearch.java
@@ -0,0 +1,28 @@
+package _9_recursion;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 4, 21, 27, 34, 65};
+        int ans = binarySearch(nums, 21, 0, nums.length - 1);
+        System.out.println(ans);
+    }
+
+    public static int binarySearch(int[] nums, int target, int start, int end){
+
+        if (start > end){
+            return -1;
+        }
+        int mid = start + (end - start) / 2;
+
+        if (nums[mid] == target){
+            return mid;
+        }
+
+        if (nums[mid] < target){
+            return binarySearch(nums, target, mid + 1, end);
+        }
+
+        return binarySearch(nums, target, start, mid - 1);
+    }
+
+}
