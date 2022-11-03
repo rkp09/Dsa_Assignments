572540544863b299a410cf54409e7e0173ec77b0
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-11-03 15:20:18 +0600

    Nth fibonacci number with golden ratio.

--- src/_9_recursion/Fibonacci.java
+++ src/_9_recursion/Fibonacci.java
@@ -2,10 +2,16 @@ package _9_recursion;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int ans = fibonacci(5);
-        System.out.println(ans);
+        System.out.println(fiboNumber(50));
     }
 
+    // Find the nth fibonacci number in fibonacci series
+    // This formula is found when analysis time complexity
+    // And the golden ratio of time complexity of nth number
+    // of fibonacci series is O(1.618^n)
+    public static int fiboNumber(int n){
+        return (int) ((1 / Math.sqrt(5)) * (Math.pow(((1 + Math.sqrt(5)) / 2), n) - Math.pow(((1 - Math.sqrt(5)) / 2), n)));
+    }
 
     // The argument going to be nth position of fibonacci series
     public static int fibonacci(int n){
