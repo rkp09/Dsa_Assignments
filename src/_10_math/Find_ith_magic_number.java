b982be8b542f6f660b6a3480d66f535601ae5a8b
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-11-03 22:59:54 +0600

    Find ith magic number.

---
+++ src/_10_math/Find_ith_magic_number.java
@@ -0,0 +1,36 @@
+package _10_math;
+
+/*
+    Magic number:
+        1 = 0 * 5^3 + 0 * 5^2 + 1 * 5^1 = 5
+        2 = 0 * 5^3 + 1 * 5^2 + 0 * 5^1 = 25
+        3 = 0 * 5^3 + 1 * 5^2 + 1 * 5^1 = 30
+        4 = 1 * 5^3 + 0 * 5^2 + 0 * 5^1 = 125
+        1 = 1 * 5^3 + 0 * 5^2 + 1 * 5^1 = 130
+
+ */
+public class Find_ith_magic_number {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 21; i++) {
+            System.out.println(magicNumber(i));
+        }
+    }
+
+    public static int magicNumber(int n){
+        int len = (int) (Math.log(n) / Math.log(2)) + 1;
+        int number = 0;
+
+        int i = 1;
+        while (i <= len){
+            int currentBit = n & 1;
+            number += currentBit * Math.pow(5, i);
+
+            // Right shifted n by 1 to get the next bit.
+            n >>= 1;
+
+            i++;
+        }
+
+        return number;
+    }
+}
