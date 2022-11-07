8b233a7106019276655bb68a42e5ee7a2542351e
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-11-07 20:16:49 +0600

    Print the number in ascending and descending way.

---
+++ src/_10_recursion/Class/Recursion_1/Print_1_to_N_Numbers.java
@@ -0,0 +1,29 @@
+package _10_recursion.Class.Recursion_1;
+
+public class Print_1_to_N_Numbers {
+    public static void main(String[] args) {
+        numbersAscDsc(5);
+    }
+
+    public static void numbers(int n){
+        if (n < 1){
+            return;
+        }
+
+        numbers(n - 1);
+
+        System.out.println(n);
+    }
+
+    public static void numbersAscDsc(int n){
+        if (n < 1){
+            return;
+        }
+
+        System.out.println(n);
+
+        numbersAscDsc(n - 1);
+
+        System.out.println(n);
+    }
+}
