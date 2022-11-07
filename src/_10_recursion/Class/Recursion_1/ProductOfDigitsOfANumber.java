554e8b94595cc6623ddb5b1584231a46e6a0d9da
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-11-07 21:07:10 +0600

    Product of the digits of the number.

---
+++ src/_10_recursion/Class/Recursion_1/ProductOfDigitsOfANumber.java
@@ -0,0 +1,15 @@
+package _10_recursion.Class.Recursion_1;
+
+public class ProductOfDigitsOfANumber {
+    public static void main(String[] args) {
+        System.out.println(digitsProduct(21));
+    }
+
+    public static int digitsProduct(int n){
+        if (n % 10 == n){
+            return n;
+        }
+
+        return (n % 10) * digitsProduct(n / 10);
+    }
+}
