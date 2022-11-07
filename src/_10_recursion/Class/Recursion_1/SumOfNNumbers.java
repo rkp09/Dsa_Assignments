36a0baf1c1ee64c98fe27fbc4f4567840cd53c00
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-11-07 20:35:46 +0600

    Sum of N numbers.

---
+++ src/_10_recursion/Class/Recursion_1/SumOfNNumbers.java
@@ -0,0 +1,15 @@
+package _10_recursion.Class.Recursion_1;
+
+public class SumOfNNumbers {
+    public static void main(String[] args) {
+        System.out.println(sum(21));
+    }
+
+    public static int sum(int n){
+        if (n == 1){
+            return 1;
+        }
+
+        return n + sum(n - 1);
+    }
+}
