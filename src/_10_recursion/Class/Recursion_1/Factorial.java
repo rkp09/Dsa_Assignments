692333b843a86063b7eb250e73542da3763424aa
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-11-07 20:30:32 +0600

    Factorial of N.

---
+++ src/_10_recursion/Class/Recursion_1/Factorial.java
@@ -0,0 +1,16 @@
+package _10_recursion.Class.Recursion_1;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+
+    public static int fact(int n){
+        // We know, 0! = 1, 1! = 1;
+        if (n < 2){
+            return 1;
+        }
+
+        return n * fact(n - 1);
+    }
+}
