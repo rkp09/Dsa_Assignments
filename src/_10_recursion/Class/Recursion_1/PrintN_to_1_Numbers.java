0153f1b91d5e6ef85871c32266718ec6f1f6a4b8
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-11-07 20:01:52 +0600

    Print n to 1 numbers.

---
+++ src/_10_recursion/Class/Recursion_1/PrintN_to_1_Numbers.java
@@ -0,0 +1,16 @@
+package _10_recursion.Class.Recursion_1;
+
+public class PrintN_to_1_Numbers {
+    public static void main(String[] args) {
+        numbers(5);
+    }
+
+    public static void numbers(int n){
+        // Base case
+        if (n < 1)
+            return;
+
+        System.out.println(n);
+        numbers(n - 1);
+    }
+}
