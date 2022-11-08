c5cd998b66b1cfa13a3c29b5956aeca1d601d601
Ridoy Chandra Dey <ridoychandradey09@gmail.com>
2022-11-08 08:49:59 +0600

    Number of steps required to reduce a number to zero.

---
+++ src/_10_recursion/Class/Recursion_1/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,28 @@
+package _10_recursion.Class.Recursion_1;
+
+public class NumberOfStepsToReduceANumberToZero {
+
+    public static void main(String[] args) {
+        System.out.println(numberOfSteps(14));
+    }
+    public static int numberOfSteps(int num) {
+        if (num == 0){
+            return 0;
+        }
+
+        return helper(num, 0);
+    }
+
+    public static int helper(int num, int steps){
+
+        if (num == 0){
+            return steps;
+        }
+
+        if ((num & 1) == 0){
+            return helper(num / 2, ++steps);
+        }
+
+        return helper(num - 1, ++steps);
+    }
+}
